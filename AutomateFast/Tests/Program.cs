c17b66214bcf887fe12b8f89e16e7aa6b3ca8152
priyadarshinitamije <priyamishti290@gmail.com>
2019-07-08 17:45:52 +1200

    final commit

--- AutomateFast/Tests/Program.cs
+++ AutomateFast/Tests/Program.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AutomateFast.Pages;
+using AutomateFast.Helpers;
+using NUnit.Framework;
 
 namespace AutomateFast
 {
+
     class Program
     {
+
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("hi");
+
+
         }
     }
 }
